4d137f791a65bb007fe4f4195bc014ad637af897
Ben Chamblee <64371981+Bench-amblee@users.noreply.github.com>
2021-02-15 22:29:23 -0600

    adding boroughs code

--- nyc_site.py
+++ nyc_site.py
@@ -3,9 +3,20 @@
 
 # In[1]:
 
-
+import pandas as pd
+from datetime import datetime
 import streamlit as st
 from PIL import Image
+import matplotlib.pyplot as plt
+import arcgis
+import sys
+from arcgis.gis import GIS
+from arcgis.raster.functions import apply, clip, remap, colormap
+from arcgis.geometry import lengths, areas_and_lengths, project
+from arcgis.geometry import Point, Polyline, Polygon, Geometry
+from arcgis.geocoding import geocode
+from arcgis.features import FeatureLayer
+gis = GIS()
 image1 = Image.open('images/nyc1.jfif')
 image2 = Image.open('images/nyc2.jfif')
 image3 = Image.open('images/nyc3.jfif')
@@ -40,3 +51,71 @@ def show_image(choice):
             st.image(graph)
 show_image(view)
 
+# boroughs
+
+area = geocode("New York City", out_sr=landsat.properties.spatialReference)[0]
+landsat.extent = area['extent']
+selected = landsat.filter_by(where="(Category = 1) AND (cloudcover <=0.05)",
+                            time=[datetime(2020, 4, 1), datetime(2020, 6, 30)],
+                             geometry=arcgis.geometry.filters.intersects(area['extent']))
+
+df = selected.query(out_fields="AcquisitionDate, GroupName, CloudCover, DayOfYear", 
+                    order_by_fields="AcquisitionDate").sdf
+df['AcquisitionDate'] = pd.to_datetime(df['AcquisitionDate'], unit='ms')
+nyc_image = landsat.filter_by('OBJECTID=3372200')
+apply(nyc_image, 'Natural Color with DRA')
+nyc_colorized = apply(nyc_image, 'NDVI Raw')
+
+boroughs = FeatureLayer('https://gisservices.its.ny.gov/arcgis/rest/services/NYS_Civil_Boundaries/FeatureServer/2')
+nh_df = pd.DataFrame.spatial.from_layer(boroughs)
+#Brooklyn
+bk_df = nh_df.loc[nh_df['NAME'] == 'Kings']
+#Staten Island
+si_df = nh_df.loc[nh_df['NAME'] == 'Richmond']
+#Manhattan
+m_df = nh_df.loc[nh_df['NAME'] == 'New York']
+#The Bronx
+tb_df = nh_df.loc[nh_df['NAME'] == 'Bronx']
+#Queens
+q_df = nh_df.loc[nh_df['NAME'] == 'Queens']
+def all_together(df, name):
+
+    def land_clip(df):
+        global shape
+        global poly
+        global masked
+        poly = df.iloc[0].SHAPE
+        shape = clip(nyc_colorized,poly)
+        shape.extent = area['extent']
+        masked = colormap(remap(shape,
+                        input_ranges=[-1,0,     # water
+                                     -0.1, 0.4, # Concrete
+                                     0.4, 1],   # Vegetation, Trees      
+                        output_values=[1, 2, 3]),
+                        colormap=[[1, 1, 255, 248], [2, 144, 148, 148], [3,14,247,22]], astype='u8')
+    land_clip(df)
+    def landcover_hist(shape,masked):
+        global hist
+        xpix = (shape.extent['xmax'] - shape.extent['xmin']) / 800
+        ypix = (shape.extent['ymax'] - shape.extent['ymin']) / 400
+
+        full_res = masked.compute_histograms(shape.extent,
+                                   pixel_size={'x':xpix, 'y': ypix})
+        total_pix = 0
+        hist = full_res['histograms'][0]['counts'][0:]
+        for i in hist[1:]:
+            total_pix += i
+    landcover_hist(shape,masked)
+    
+    def pie_chart(hist, name):
+        colors=['#01FFF8','#909494','#0EF716']
+        labels=['Water', 'Concrete', 'Green Cover']
+        sizes = hist[1:]
+        plt.pie(sizes,autopct='%1.1f%%',colors=colors,shadow=True)
+        plt.title('Landcover of '+ str(name))
+        plt.legend(labels,loc = 'right',bbox_to_anchor=(1.45, 0.75), ncol=1)
+        plt.show()
+    pie_chart(hist, name)
+    return masked
+
+all_together(bk_df,'Brooklyn')
