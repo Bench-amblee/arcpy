d84efda6193a15a5e0179b0954d0f75a6c03865a
Ben Chamblee <64371981+Bench-amblee@users.noreply.github.com>
2021-02-17 21:00:26 -0600

    Update nyc_site.py

--- nyc_site.py
+++ nyc_site.py
@@ -14,6 +14,37 @@ fc_sat_3 = Image.open('images/fc_sat_1999.jfif')
 fc_sat_4 = Image.open('images/fc_sat_2010.jfif')
 fc_sat_5 = Image.open('images/fc_sat_2015.jfif')
 fc_sat_6 = Image.open('images/fc_sat_2020.jfif')
+b_sat_1 = Image.open('images/b_sat_1975.jfif')
+b_sat_2 = Image.open('images/b_sat_1988.jfif')
+b_sat_3 = Image.open('images/b_sat_1999.jfif')
+b_sat_4 = Image.open('images/b_sat_2010.jfif')
+b_sat_5 = Image.open('images/b_sat_2015.jfif')
+b_sat_6 = Image.open('images/b_sat_2020.jfif')
+q_sat_1 = Image.open('images/q_sat_1975.jfif')
+q_sat_2 = Image.open('images/q_sat_1988.jfif')
+q_sat_3 = Image.open('images/q_sat_1999.jfif')
+q_sat_4 = Image.open('images/q_sat_2010.jfif')
+q_sat_5 = Image.open('images/q_sat_2015.jfif')
+q_sat_6 = Image.open('images/q_sat_2020.jfif')
+m_sat_1 = Image.open('images/m_sat_1975.jfif')
+m_sat_2 = Image.open('images/m_sat_1988.jfif')
+m_sat_3 = Image.open('images/m_sat_1999.jfif')
+m_sat_4 = Image.open('images/m_sat_2010.jfif')
+m_sat_5 = Image.open('images/m_sat_2015.jfif')
+m_sat_6 = Image.open('images/m_sat_2020.jfif')
+tb_sat_1 = Image.open('images/tb_sat_1975.jfif')
+tb_sat_2 = Image.open('images/tb_sat_1988.jfif')
+tb_sat_3 = Image.open('images/tb_sat_1999.jfif')
+tb_sat_4 = Image.open('images/tb_sat_2010.jfif')
+tb_sat_5 = Image.open('images/tb_sat_2015.jfif')
+tb_sat_6 = Image.open('images/tb_sat_2020.jfif')
+si_sat_1 = Image.open('image/si_sat_1975.jfif')
+si_sat_2 = Image.open('image/si_sat_1988.jfif')
+si_sat_3 = Image.open('image/si_sat_1999.jfif')
+si_sat_4 = Image.open('image/si_sat_2010.jfif')
+si_sat_5 = Image.open('image/si_sat_2015.jfif')
+si_sat_6 = Image.open('image/si_sat_2020.jfif')
+
 
 city_dict = {
   'Full_City':{
@@ -24,33 +55,40 @@ city_dict = {
     'Greenery':[1975,1988,1999,2010,2015,2020],
     'Full':[1975,1988,1999,2010,2015,2020]},
     'Brooklyn':{
-      'Satellite':[1975,1988,1999,2010,2015,2020],
+      'Satellite':{1975:b_sat_1,1988:b_sat_2,1999:b_sat_3,2010:b_sat_4,2015:b_sat_5,2020:b_sat_6},
       'NDVI':[1975,1988,1999,2010,2015,2020],
       'Water':[1975,1988,1999,2010,2015,2020],
       'Buildings':[1975,1988,1999,2010,2015,2020],
       'Greenery':[1975,1988,1999,2010,2015,2020],
       'Full':[1975,1988,1999,2010,2015,2020]},
       'Queens':{
-        'Satellite':[1975,1988,1999,2010,2015,2020],
+        'Satellite':{1975:q_sat_1,1988:q_sat_2,1999:q_sat_3,2010:q_sat_4,2015:q_sat_5,2020:q_sat_6},
         'NDVI':[1975,1988,1999,2010,2015,2020],
         'Water':[1975,1988,1999,2010,2015,2020],
         'Buildings':[1975,1988,1999,2010,2015,2020],
         'Greenery':[1975,1988,1999,2010,2015,2020],
         'Full':[1975,1988,1999,2010,2015,2020]},
         'Manhattan':{
-          'Satellite':[1975,1988,1999,2010,2015,2020],
+          'Satellite':{1975:m_sat_1,1988:m_sat_2,1999:m_sat_3,2010:m_sat_4,2015:m_sat_5,2020:m_sat_6},
           'NDVI':[1975,1988,1999,2010,2015,2020],
           'Water':[1975,1988,1999,2010,2015,2020],
           'Buildings':[1975,1988,1999,2010,2015,2020],
           'Greenery':[1975,1988,1999,2010,2015,2020],
           'Full':[1975,1988,1999,2010,2015,2020]},
           'The_Bronx':{
-            'Satellite':[1975,1988,1999,2010,2015,2020],
+            'Satellite':{1975:tb_sat_1,1988:tb_sat_2,1999:tb_sat_3,2010:tb_sat_4,2015:tb_sat_5,2020:tb_sat_6},
             'NDVI':[1975,1988,1999,2010,2015,2020],
             'Water':[1975,1988,1999,2010,2015,2020],
             'Buildings':[1975,1988,1999,2010,2015,2020],
             'Greenery':[1975,1988,1999,2010,2015,2020],
-            'Full':[1975,1988,1999,2010,2015,2020]}
+            'Full':[1975,1988,1999,2010,2015,2020]},
+  'Staten_Island':{
+    'Satellite':{1975:si_sat_1,1988:si_sat_2,1999:si_sat_3,2010:si_sat_4,2015:si_sat_5,2020:si_sat_6},
+    'NDVI':[1975,1988,1999,2010,2015,2020],
+    'Water':[1975,1988,1999,2010,2015,2020],
+    'Buildings':[1975,1988,1999,2010,2015,2020],
+    'Greenery':[1975,1988,1999,2010,2015,2020],
+    'Full':[1975,1988,1999,2010,2015,2020]}
         }
 full_city_dict = {'Satellite': image1,'NDVI Filter': image2,'City Boundary Cutout': image3,
               'Water': image5, 'Concrete': image6, 'Greenery': image7, 'Final': image4}
@@ -73,6 +111,10 @@ side2 = st.sidebar.selectbox(
 def home_page(x,y):
   if x == 'Full City':
     x = 'Full_City'
+  if x == 'The Bronx':
+    x = 'The_Bronx'
+  if x == 'Staten Island':
+    x = 'Staten_Island'
   z = st.select_slider('Year',options= [1975, 1988, 1999, 2010, 2015, 2020])
   st.image(city_dict[x][y][z])
   st.write('New York City in ' +str(z))
