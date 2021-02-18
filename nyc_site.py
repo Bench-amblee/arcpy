ed1e690d842a19d38ccfa71d6d89a7e102ba4024
Ben Chamblee <64371981+Bench-amblee@users.noreply.github.com>
2021-02-17 21:16:57 -0600

    Update nyc_site.py

--- nyc_site.py
+++ nyc_site.py
@@ -111,7 +111,6 @@ def home_page(x,y):
   z = st.select_slider('Year',options= [1975, 1988, 1999, 2010, 2015, 2020])
   #st.image(city_dict[x][y][z],width=1500)
   col1, col2 = st.beta_columns(2)
-  original = Image.open(image)
   col1.header("Original")
   col1.image(city_dict[x][y][z],width=1500)
   col2.header("")
