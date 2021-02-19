1749110adbb834264175322311c2a08f5dd2ef72
Ben Chamblee <64371981+Bench-amblee@users.noreply.github.com>
2021-02-19 14:19:21 -0600

    Update nyc_site.py

--- nyc_site.py
+++ nyc_site.py
@@ -153,7 +153,6 @@ def home_page(x,y):
       df = pd.read_excel(Hists,x_)
       data = [df['Name'], df[a]]
       df3 = pd.concat(data, axis=1)
-      df3.setindex('Name')
       st.bar_chart(df3)
   #col1, col2, col3 = st.beta_columns(3)
   #original = Image.open('images/white.jpg')
